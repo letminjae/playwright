f121ee7df6bc7f451499e3c075f53f16ca1752b4
차민재 <cmjj0824@naver.com>
2024-11-14 21:29:54 +0900

    Test : 페이지 정상로딩 테스트, 전체 글 테스트, 블로그 다크모드 전환 테스트 자동화 추가

--- tests/UIbasics.spec.ts
+++ tests/UIbasics.spec.ts
@@ -1,15 +1,34 @@
 import { test, expect } from "@playwright/test";
 
 test.describe("나의 티스토리 블로그 UI 테스트", () => {
-  test.beforeEach(async({page}) => {
+  test.beforeEach(async ({ page }) => {
     await page.goto("/");
-  })
+  });
+
+  test("페이지 정상적 로딩", async ({ page }) => {
+    await expect(page).toHaveURL("https://letminjae.tistory.com/");
+    await expect(page).toHaveTitle(/개발과 품질 그 사이/); // 타이틀이 특정 문자열을 포함하는지 확인
+  });
+
+  test("전체 글 테스트", async ({ page }) => {
+    const allTitle = page.getByText("전체 글");
+    await expect(allTitle).toBeVisible();
+  });
 
   test(`페이지 이동 후 검색창에 요소 입력`, async ({ page }) => {
     await page.locator("input#keyword").fill("QA");
+    await page.locator("input#keyword").press("Enter");
+    await expect(page).toHaveURL("/search/QA");
   });
 
-  test(`사이드 바 타이틀 클릭`, async ({ page }) => {
-    await page.getByRole('link', { name: "개발과 품질 그 사이" }).click();
+  test("블로그 다크모드 전환", async ({ page }) => {
+    const themeButton = await page.locator("button.theme").click();
+
+    // 특정 요소의 배경색이 다크 모드 색상으로 변경되었는지 확인 (예: body의 배경색)
+    const backgroundColor = await page.evaluate(() => {
+      return window.getComputedStyle(document.body).backgroundColor;
+    });
+    console.log(backgroundColor);
+    expect(backgroundColor).toBe('rgb(30, 31, 33)')
   });
 });
