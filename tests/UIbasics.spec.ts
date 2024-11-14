b84ad0602eadc7128a416688a2c330095c6999d0
차민재 <cmjj0824@naver.com>
2024-11-14 20:39:22 +0900

    chore : UI test describe 설정, 테스트 수행 이전 URL 자동 설정

--- tests/UIbasics.spec.ts
+++ tests/UIbasics.spec.ts
@@ -1,5 +1,15 @@
-const {test} = require('@playwright/test');
+import { test, expect } from "@playwright/test";
 
-test(`브라우저 없이 페이지 이동 테스트`, async({page}) => {
-  await page.goto(`letminjae.tistory.com`);
+test.describe("나의 티스토리 블로그 UI 테스트", () => {
+  test.beforeEach(async({page}) => {
+    await page.goto("/");
+  })
+
+  test(`페이지 이동 후 검색창에 요소 입력`, async ({ page }) => {
+    await page.locator("input#keyword").fill("QA");
+  });
+
+  test(`사이드 바 타이틀 클릭`, async ({ page }) => {
+    await page.getByRole('link', { name: "개발과 품질 그 사이" }).click();
+  });
 });
