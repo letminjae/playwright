988cd9b6cda076f69fefb202f9b83f3a6ab71218
차민재 <cmjj0824@naver.com>
2024-11-18 20:32:00 +0900

    Test : E2E Test 과제 테스트 케이스 추가

---
+++ tests/E2ETestHomework.spec.ts
@@ -0,0 +1,12 @@
+const { test, expect } = require("@playwright/test");
+
+test("E2E Test 과제", async ({ page }) => {
+  //js file- Login js, DashboardPage
+  const email = "cmjj0824@naver.com";
+  const productName = "zara coat 3";
+  const products = page.locator(".card-body");
+  await page.goto("https://rahulshettyacademy.com/client");
+  await page.locator("#userEmail").fill(email);
+  await page.locator("#userPassword").type("Qwer1234!");
+  await page.locator("[value='Login']").click();
+});
