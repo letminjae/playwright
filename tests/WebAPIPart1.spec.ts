4c0e4a73ce0dc266d8ba943f1eee41ae7309f095
차민재 <cmjj0824@naver.com>
2024-11-25 21:48:08 +0900

    Chore : API Test beforeAll - 로그인 및 json token 테스트 셋팅

--- tests/WebAPIPart1.spec.ts
+++ tests/WebAPIPart1.spec.ts
@@ -1,6 +1,23 @@
-import { test, expect } from "@playwright/test";
+import { test, expect, request } from "@playwright/test";
 
+const loginPayload = {
+  userEmail: "cmjj0824@naver.com",
+  userPassword: "Qwer1234!",
+};
 
 test.describe("Web API Test", () => {
-  
+  test.beforeAll(async () => {
+    // API Test Setup - login and response.json
+    const apiContext = await request.newContext();
+    const loginResponse = await apiContext.post(
+      `https://rahulshettyacademy.com/api/ecom/auth/login/`,
+      {
+        data: loginPayload,
+      }
+    );
+    expect(loginResponse.ok()).toBeTruthy(); // 200, 201..
+    const loginResponseJson = await loginResponse.json();
+    const token = loginResponseJson.token;
+    console.log(token);
+  });
 });
