9e40efa48a0a89f4d7402c28dacb5535f1fb2148
차민재 <cmjj0824@naver.com>
2024-11-30 16:12:47 +0900

    Chore : download 이벤트 대기 코드 추가

--- tests/Upload-Download.spec.ts
+++ tests/Upload-Download.spec.ts
@@ -4,6 +4,7 @@ test("엑셀 업로드 및 다운로드 테스트", async ({ page }) => {
   await page.goto(
     "https://rahulshettyacademy.com/upload-download-test/index.html"
   );
+  const downloadPromise = await page.waitForEvent("download");
   await page.getByRole("button", { name: "Download" }).click();
-
+  await downloadPromise;
 });
