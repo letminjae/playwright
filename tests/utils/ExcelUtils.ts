57d8639b586e22ef1433a94935491c81dd8e194b
차민재 <cmjj0824@naver.com>
2024-11-30 15:39:54 +0900

    Add : ExcelUtils.ts 파일 추가, Excel 파일 관련 정보 추출용

---
+++ tests/utils/ExcelUtils.ts
@@ -0,0 +1,16 @@
+import ExcelJs from "exceljs";
+
+const workbook = new ExcelJs.Workbook();
+workbook.xlsx
+  .readFile("/Users/chaminjae/Download/excelDownloadTest.xlsx")
+  .then(function () {
+    const worksheet = workbook.getWorksheet("Sheet1");
+    worksheet?.eachRow((row, rowNumber) => {
+      row.eachCell((cell, colNumber) => {
+        // console.log(cell.value);
+        if (cell.value === "apple"){
+          console.log(rowNumber)
+        }
+      });
+    });
+  });
