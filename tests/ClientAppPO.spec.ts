11452a16b85e560e482b34852034df58f33d6025
차민재 <cmjj0824@naver.com>
2024-12-04 20:51:29 +0900

    Refactor : 하드코딩 데이터 TestData 객체에 보관 후, 재사용화

--- tests/ClientAppPO.spec.ts
+++ tests/ClientAppPO.spec.ts
@@ -1,28 +1,26 @@
 import { expect, test } from "@playwright/test";
 import POManager from "../page_object/POManager";
+import data from "./utils/TestData";
 
 test.describe("POM 테스트", () => {
   test("E2E Test", async ({ page }) => {
-    const username = "cmjj0824@naver.com";
-    const password = "Qwer1234!";
-    const productName = "ZARA COAT 3";
-    const products = page.locator(".card-body");
+    const jsonData = JSON.parse(JSON.stringify(data));
 
     const poManager = new POManager(page);
 
     // 로그인 PO
     const loginPage = poManager.getLoginPage();
     await loginPage.goTo();
-    await loginPage.validLogin(username, password);
+    await loginPage.validLogin(data.username, data.password);
 
     // 대시보드 PO
     const dashboardPage = poManager.getDashboardPage();
-    await dashboardPage.searchProductAddCart(productName);
+    await dashboardPage.searchProductAddCart(data.productName);
     await dashboardPage.navigateToCart();
 
     // 카트 PO
     const cartPage = poManager.getCartPage();
-    await cartPage.VerifyProductIsDisplayed(productName);
+    await cartPage.VerifyProductIsDisplayed(data.productName);
     await cartPage.Checkout();
 
     // 주문 PO
@@ -32,6 +30,7 @@ test.describe("POM 테스트", () => {
 
     await dashboardPage.navigateToOrders();
 
+    // 주문 내역 PO
     const ordersHistoryPage = poManager.getOrdersHistoryPage();
     await ordersHistoryPage.searchOrderAndSelect(orderId);
     const fetchedOrderId = await ordersHistoryPage.getOrderId();
---
+++ tests/utils/TestData.ts
@@ -0,0 +1,13 @@
+interface Data {
+  username: string;
+  password: string;
+  productName: string;
+}
+
+const data: Data = {
+  username: "cmjj0824@naver.com",
+  password: "Qwer1234!",
+  productName: "ZARA COAT 3",
+};
+
+export default data;
